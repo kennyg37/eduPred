96494691e0eb38aa3257e08914690f0f5a421224
kennyg37 <k.ganza@alustudent.com>
2024-11-30 13:36:03 +0200

    fixing issues with tf on render

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, jsonify, redirect, request, send_from_directory
 from dotenv import load_dotenv
 import numpy as np
 from src.prediction import predict, scale_features, scale_student_data
-# from src.preprocessing import scale_data
 from src.retraining import retrain_model, retrain_student_model
 from flask_swagger_ui import get_swaggerui_blueprint
 from werkzeug.utils import secure_filename
@@ -12,20 +11,23 @@ load_dotenv()
 
 app = Flask(__name__)
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 port = int(os.getenv("PORT", 5000))
-API_URL = '/static/swagger.json'
+API_URL = os.getenv("API_URL", '/static/swagger.json')
 
-UPLOAD_FOLDER = '/tmp/uploads'
+UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", '/tmp/uploads')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)  
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 swaggerui_bluprint = get_swaggerui_blueprint('/docs', API_URL)
 app.register_blueprint(swaggerui_bluprint)
 
-# Load the model when the app starts
-MODEL_PATH = './model/edupred.keras'
+STUDENT_TEMP_MODEL_PATH = os.getenv("STUDENT_TEMP_MODEL_PATH", '/tmp/student_gpa_model.keras')
+ED_TEMP_MODEL_PATH = os.getenv("ED_TEMP_MODEL_PATH", '/tmp/edupred.keras')
+
+MODEL_PATH = os.getenv("MODEL_PATH", './model/edupred.keras')
 model = None
-STUDENT_MODEL_PATH = './model/student_model.keras'
+STUDENT_MODEL_PATH = os.getenv("STUDENT_MODEL_PATH", './model/student_model.keras')
 student_model = None
 
 @app.route('/', methods=['GET'])
@@ -61,9 +63,9 @@ def retrain_endpoint():
     """Endpoint to retrain the model."""
     global model
     try:
-        # Retrain the model using the retrain_model function
+        # Retrain the mode using the retrain_model funtion
         new_model = retrain_model()
-        new_model.save(MODEL_PATH)
+        new_model.save(STUDENT_TEMP_MODEL_PATH)
         model = new_model
         return jsonify({"message": "Model retrained successfully."})
     except Exception as e:
@@ -112,7 +114,7 @@ def retrain_student_endpoint():
             file.save(file_path)
         else:
             file_path = './data/Student_performance_data _.csv'
-        # Retrain the mode using the retrain_model funtion
+        # Retrain the mode using the retrain_model function
         new_model = retrain_student_model(file_path)
         new_model.save(STUDENT_MODEL_PATH)
         student_model = new_model
@@ -122,4 +124,3 @@ def retrain_student_endpoint():
 
 if __name__ == "__main__":
     app.run(port=port, debug=True)
-    
